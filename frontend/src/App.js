c80b8ff3b02b72c9063169d1558550f48082d196
Vrána Daniel <vrad00@vse.cz>
2018-09-25 16:21:18 +0000

    intial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const InputWithLabel = ({label}) =>(
+  <div>
+    <label>{label}:</label>
+    <input type = "text"/>
+  </div>
+);
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +17,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
+        <InputWithLabel label="neco"/>
+        <Hello name= "One" helloGreeting="Hola"/>
+        <Hello name= "Two"/>
+        <Hello name= "Three"/>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,5 +30,16 @@ class App extends Component {
     );
   }
 }
+
+class Hello extends Component{
+  render(){
+    return(
+      <div>
+        {this.props.helloGreeting || 'Hello'}, {this.props.name}!
+      </div>
+    );
+  }
+}
+
 
 export default App;
